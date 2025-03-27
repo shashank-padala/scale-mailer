fd70080409d4b090e8bfa1fb9967f8d9f48af19b
shashank-padala <shashank.padala24@gmail.com>
2025-03-27 09:22:35 -0400

    added vercel json file to ignore next js routing and adjust to vite+react

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    fs: {
+      strict: false,
+    },
+    // fallback for dev (already works locally, but safe to keep)
+    historyApiFallback: true,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
